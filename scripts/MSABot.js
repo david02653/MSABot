ff53047115834e56369a0634b92d9e99533ef5a8
david02653 <david02653@gmail.com>
2020-05-15 22:08:23 +0800

    debug msg

--- scripts/MSABot.js
+++ scripts/MSABot.js
@@ -133,7 +133,6 @@ var initBot = function(robot)
             robot.brain.set("reconnect_count", 0);
             robot.brain.set("bots", bots);
 			robot.brain.set('reset_check', 0);
-			console.log("add testing message in console");
             console.log(bots);
             db.close();
         });
@@ -148,7 +147,8 @@ var newBot = function(token, name, robot, team_name, isInstall)
         token: token, // Add a bot https://my.slack.com/services/new/bot and put the token 
         name: name,
         disconnect: true
-    });
+	});
+	console.log(bot);
     bot.on('start', function() {
         // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
         var welcome = ['我又回來了。請先停看聽。', '振作起來好嗎。我剛回來。', '我的老天鵝。我回來了。', '我回來了。', '我回來了。派對收攤。', '我又來了。期待您的使用 ( ͡° ͜ʖ ͡°)', '安安', '嗖。 我剛剛著陸下來。','我剛回來。 可以幫我補血嗎？','挑戰者來了，我 來 也！','我來了。 請給我一罐啤酒。','嘿！快聽！我回來了！', '我剛回來。 似乎太 OP - 請 nerf 一下。', '一個人走很危險，跟我一起走吧！', '我上來了！我又下去了！我又上來了！怎麼樣！我可以一直上來下來！很厲害吧！', '槓上開花加一台（主機）'];
